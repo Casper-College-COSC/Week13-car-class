6c273681a040160abc8a476f6384b43a253ebed0
Charlotte Gruner <60362002+charlottegruner@users.noreply.github.com>
2025-04-23 11:56:27 -0600

    Add files via upload

---
+++ car_class.cpp
@@ -0,0 +1,26 @@
+main.cpp:
+--------
+#include <iostream>
+using namespace std; 
+
+/* Create a Car class with private member fields 
+   for make, model, and speed.  Write getters and setters 
+   for the fields.  Write an 'accelerate()' member function that 
+   adds 5 to the speed field. Write a 'printInfo()' member function that
+   will print out all the fields in the object. 
+   Test the code out by making a Car
+   object in main and calling the member functions.  
+  */
+  
+
+int main()
+{
+    
+    return 0;
+}
+
+makefile:
+--------
+all:
+        g++ main.cpp
+
